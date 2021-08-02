5a4aa251e09491a3e6d0333cff5502005a4e8b2d
donc-py <67079670+donc-py@users.noreply.github.com>
2021-08-02 00:28:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Telegram Radio Player Bitpattern
+
+A Telegram UserBot to Play Radio in Channel or Group Voice Chats.
+
+(https://t.me/asdsaada) Channel.
+
+
+
+
+## Requirements
+
+- Python 3.6 or higher.
+- A
+  [Telegram API key](https://docs.pyrogram.org/intro/quickstart#enjoy-the-api)
+  and a Telegram account.
+- [FFmpeg Python](https://www.ffmpeg.org/)
+- Telegram [String Session](http://t.me/genStr_robot) of the account.
+- Userbot Needs To Be Admin In The Channel or Group.
---
+++ plugins/commands.py
@@ -0,0 +1,63 @@
+
+
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
+
+
+
+HOME_TEXT = "👋🏻 **Hi [{}](tg://user?id={})**,\n\nI'm **Radio Player Bot** \nI Can Play Radio/Stream Music In Channels & Groups 24x7 Nonstop. Made with ❤️ By @AsmSafone!"
+HELP = """🏷️ **Need Help?** 🤔
+__(Join @SafoTheBot For Support)__
+
+🏷️ **Common Commands**:
+\u2022 `/play` reply to an audio to play or queue it
+\u2022 `/help` shows help for commands
+\u2022 `/playlist` shows the playlist
+\u2022 `/current` shows playing time of current track
+\u2022 `/song` [song name] download the song as audio
+
+🏷️ **Admin Commands**:
+\u2022 `/skip` [n] skip current or n where n >= 2
+\u2022 `/join` join voice chat of current group
+\u2022 `/leave` leave current voice chat
+\u2022 `/vc` check which VC is joined
+\u2022 `/stop` stop playing music
+\u2022 `/radio` start radio stream
+\u2022 `/stopradio` stop radio stream
+\u2022 `/replay` play from the beginning
+\u2022 `/clean` remove unused RAW PCM files
+\u2022 `/pause` pause playing music
+\u2022 `/resume` resume playing music
+\u2022 `/mute` mute the VC userbot
+\u2022 `/unmute` unmute the VC userbot
+\u2022 `/restart` restart the bot
+
+🏷️ **Developer: @I_Am_Only_One_1** 👑
+"""
+
+
+@Client.on_message(filters.command('start'))
+async def start(client, message):
+    buttons = [
+        [
+        InlineKeyboardButton('CHANNEL', url='https://t.me/AsmSafone'),
+        InlineKeyboardButton('SUPPORT', url='https://t.me/SafoTheBot'),
+    ],
+    [
+        InlineKeyboardButton('MORE BOTS', url='https://t.me/AsmSafone/12'),
+        InlineKeyboardButton('SOURCE CODE', url='https://github.com/AsmSafone/RadioPlayerV2'),
+    ],
+    [
+        InlineKeyboardButton('⚙️ HELP ⚙️', callback_data='help'),
+        
+    ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply(HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
+
+
+
+@Client.on_message(filters.command("help"))
+async def show_help(client, message):
+    await message.reply_text(HELP)
